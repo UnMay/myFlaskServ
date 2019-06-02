321282de543d53c2d816a20a684d5de13ee2d42f
UnMay <necromant650@ya.ru>
2019-06-02 12:47:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -126,5 +126,5 @@ if __name__ == '__main__':
         print(str(e))
         clf = None
 
-    app.run(host='0.0.0.0', port=port)
-    #app.run()
+    #app.run(host='0.0.0.0', port=port)
+    app.run()
