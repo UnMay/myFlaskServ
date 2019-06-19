b5d7f2f4055230e0ceadb6191836aa8efaab3f97
UnMay <necromant650@ya.ru>
2019-06-19 11:51:30 +0500

    Update myproject.py

--- myproject.py
+++ myproject.py
@@ -28,7 +28,7 @@ pca = pickle.load(open(pca_filename, 'rb'))
 #предсказание и получение данных из ВК
 def user_predict(datavk):
     me=pd.DataFrame()
-    for s_id in datavk[0][1].tolist():
+    for s_id in datavk[0][1]:
             try:
                 user_id = datavk[0][0]
                 name = None
