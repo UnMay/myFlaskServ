6a1ce0296606b49a7d6ef44fda47d59b2e9f406c
UnMay <necromant650@ya.ru>
2019-06-19 10:34:28 +0500

    Update myproject.py

--- myproject.py
+++ myproject.py
@@ -9,8 +9,6 @@ import shutil
 import time
 import traceback
 from flask_cors import CORS, cross_origin
-import vk
-#test
 
 app = Flask(__name__)
 cros = CORS(app)
@@ -27,22 +25,14 @@ clf = pickle.load(open(clf_filename, 'rb'))
 pca_filename = 'PCA.pkl'
 pca = pickle.load(open(pca_filename, 'rb'))
 
-#авторизация в ВК
-session = vk.AuthSession(app_id='6729196', user_login='jeka87@e1.ru', user_password='P@ssword1987')
-api = vk.API(session)
-
 #предсказание и получение данных из ВК
-def user_predict(user_ids):
+def user_predict(datavk):
     me=pd.DataFrame()
-    user_info = api.users.get(user_ids=user_ids, extended='1', v='5.87')
-    user_id = user_info[0]['id']
-    subscriptions = api.users.getSubscriptions(user_id=user_id, extended='1', v='5.87')
-    subscriptions = subscriptions['items']
     for subscription in subscriptions:
             try:
-                user_id = user_id
-                name = subscription['name']
-                group_id=subscription['id']
+                user_id = datavk[0][0]
+                name = None
+                group_id=datavk[1]
                 me = me.append({'user_id': user_id, 'name': name, 'group_id':group_id}, ignore_index=True)
             except:
                 pass
@@ -57,12 +47,18 @@ def user_predict(user_ids):
     me_pred = clf.predict(test_api_data_reduced)
     return me_pred
 
+@app.route("/")
+def hello():
+    return "<h1 style='color:red'>server ok!</h1>"
+
 #веб-часть
 @app.route('/')
 def predict():
         try:
-            user_id = request.args.get('user_ids')
-            result = user_predict(user_id)[0]
+            json_ = request.json
+            datavk = pd.DataFrame(json_)
+            
+            result = user_predict(datavk)[0]
             if result=='INFO':
                 output=0
             elif result=='IGUP':
@@ -75,7 +71,6 @@ def predict():
             return jsonify({'error': str(e), 'trace': traceback.format_exc()})
 
 if __name__ == "__main__":
-    app.run()
-    #app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
 
 
